888336183a9b0a6960b00c6d29719f17e0c1f537
Maciek <maciek.cholewa.vip@gmail.com>
2022-10-21 19:49:50 +0200

    Add validation input tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,34 @@
 #include "gtest/gtest.h"
+#include "../src/InputValidation.hpp"
+
+TEST(ValidationInputTests, TooMuchArgumentsShouldThrow)
+{
+   ASSERT_THROW(validateLengthOfInput(3), std::invalid_argument);
+}
+
+TEST(ValidationInputTests, PathNotSpecifiedShouldThrow)
+{
+   ASSERT_THROW(validateLengthOfInput(1), std::invalid_argument);
+}
+
+TEST(ValidationInputTests, CorrectNumberOfArgumentsShoulNotThrow)
+{
+   ASSERT_NO_THROW(validateLengthOfInput(2));
+}
+
+TEST(ValidationInputTests, IncorrectPathShouldThrow)
+{ 
+    ASSERT_THROW(validatePath("dasdsadasdasddba"), std::invalid_argument);
+}
+
+TEST(ValidationInputTests, CorrectPathShoulNotThrow)
+{
+   ASSERT_NO_THROW(validatePath("/"));
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
