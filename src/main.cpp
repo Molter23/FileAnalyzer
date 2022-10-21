2a699f74901dafa5109f462aa9bbf7f421a85cbc
Maciek <maciek.cholewa.vip@gmail.com>
2022-10-21 18:42:21 +0200

    Refactor validate method
    Add method, which collects all the files, to calculater lines later on

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,56 @@ void usage(const std::string& programName)
     std::cout << "\t" << programName << " [path to directory]" << std::endl << std::endl;
 }
 
-void validateInput(int argc, char** argv)
+void validateLengthOfInput(int argc)
 {
-    try{
-        if(argc > 2)
-            throw std::invalid_argument("Too many arguments spcified.");
+    if(argc > 2)
+        throw std::invalid_argument("Too many arguments spcified.");
         
-        if(argc == 1)
-            throw std::invalid_argument("Path not spcified.");
+    if(argc == 1)
+        throw std::invalid_argument("Path not spcified.");
 
-        std::string dirName = argv[1];
-
-        if(!fs::is_directory(fs::status(dirName)))
-            throw std::invalid_argument("Given argument is not a path to directory.");
-    } 
-    catch(std::invalid_argument const& e)
-    {   
-        std::cerr << e.what() << std::endl;
-        std::string programName = argv[0];
-        usage(programName);
-    }       
 }
 
+void validatePath(const std::string& dirName)
+{
+    if(!fs::is_directory(fs::status(dirName)))
+        throw std::invalid_argument("Given argument is not a path to directory.");
+}
 
+std::string validateInput(int argc, char** argv)
+{
+    try{
+        validateLengthOfInput(argc);
+
+        std::string dirName = argv[1];
+        validatePath(dirName);
+
+    } catch(const std::invalid_argument& e) {   
+        std::cout << e.what() << std::endl;
+        std::string programName = argv[0];
+        usage(programName);
+        exit(EXIT_FAILURE);
+    }       
+    
+    return argv[1];
+}
+
+void collectAllFiles(const std::string& dirName, std::size_t capacityToReserve = 1000)
+{
+    std::vector<fs::directory_entry> fillesInAllDirectories;
+    fillesInAllDirectories.reserve(capacityToReserve);
+    
+    for (auto const& path : fs::recursive_directory_iterator(dirName))
+    {
+        if(fs::is_regular_file(fs::status(path))){    
+            fillesInAllDirectories.emplace_back(std::move(path));
+        }
+    }
+}
 
 
 int main(int argc, char** argv)
 {   
-    validateInput(argc, argv);
-
-
-
-    /*for (auto const& dir_entry : fs::recursive_directory_iterator("/"))
-    {
-        std::cout << dir_entry << '\n';
-    }*/
-
+    auto dirName = validateInput(argc, argv);
+    collectAllFiles(dirName);
 }
